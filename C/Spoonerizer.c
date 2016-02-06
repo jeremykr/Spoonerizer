8617d639e68070142baf3e0fa31a520f1a63b6d3
Jeremy Krenbrink <jeremy.krenbrink@gmail.com>
2016-02-06 14:53:27 -0800

    Added linked list to C code

--- C/Spoonerizer.c
+++ C/Spoonerizer.c
@@ -3,45 +3,79 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define INPUT_SIZE 5000
-#define BUFF_SIZE 50
-#define TOKENS_SIZE 100
+#define INPUT_SIZE 100
 
 const char consonants[] = "bcdfghjklmnpqrstvwxyz";
 const char vowels[] = "aeiou";
 
-int main() {
-	char input[INPUT_SIZE], buff[BUFF_SIZE];
-	char* tokens[TOKENS_SIZE];
-	char c;
-	int i, start_index, end_index;
-	int tokens_index = 0;
+typedef struct Node {
+	char* data;
+	struct Node* next;
+} Node;
+
+typedef struct List {
+	Node* head;
+	Node* tail;
+	int size;
+} List;
+
+List* init_list() {
+	List* L = (List*)malloc(sizeof(List*));
+	L->head = NULL;
+	L->tail = NULL;
+	L->size = 0;
+	return L;
+}
+
+Node* init_node() {
+	Node* N = (Node*)malloc(sizeof(Node*));
+	N->data = NULL;
+	N->next = NULL;
+	return N;
+}
+
+void append(List* L, char* s) {
+	Node* N = init_node();
+	char* s_copy = (char*)malloc(sizeof(s));
+	strcpy(s_copy, s);
+	N->data = s_copy;
 	
-	// Initialize tokens array
-	for (i = 0; i < TOKENS_SIZE; i++) {
-		tokens[i] = (char *)malloc(BUFF_SIZE);
+	if (!L->size) {
+		L->head = L->tail = N;
+	} else {
+		L->tail->next = N;
+		L->tail = N;
 	}
+	N->next = NULL;
+	L->size++;
+}
+
+char* to_lower(char* s) {
+/* Converts all characters in a string to lowercase. */
+	char* p = s;
+	for (; *p; p++) {
+		if ((*p) > 64 && (*p) < 91) {
+			(*p) += 32;
+		}
+	}
+	return s;
+}
+
+int main() {
+	char* input = (char*)malloc(INPUT_SIZE);
+	List* input_list = init_list();
+	Node* np;
+	
 	
 	printf("Input some words: ");
 	
-	if (fgets(input, sizeof(input), stdin) == NULL) {
+	if (fgets(input, INPUT_SIZE, stdin) == NULL) {
 		printf("Input error: input string too long.\n");
 		return 1;
 	}
 	
-	for (i = start_index = 0; c; i++) {
-		c = input[i];
-		if (c == ' ' || c == '\n') {
-			end_index = i;
-			memcpy(tokens[tokens_index], &input[start_index], end_index - start_index);
-			start_index = i + 1;
-		}
-	}
-	
-	for (i = 0; i < 2; i++) {
-		printf("%s\n", tokens[i]);
-	}
-	
+	input = to_lower(input);
 	printf("%s", input);
-	return 0;
+	printf("List size: %d\n", input_list->size);
+	
 }

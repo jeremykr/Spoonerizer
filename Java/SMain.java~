869f95d3e28b49f1be2da859813db7dbf99162fb
Jeremy Krenbrink <jeremy.krenbrink@gmail.com>
2016-02-06 20:35:42 -0800

    Added Java implementation

---
+++ Java/SMain.java~
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.Scanner;
+
+class SMain {
+
+	public static void main(String[] args) {
+	
+		Scanner scan = new Scanner(System.in);
+		Spoonerizer spooner = new Spoonerizer();
+		
+		System.out.print("Enter some words: ");
+		String input = scan.nextLine();
+		System.out.println(spooner.spoon(input));
+		
+	}
+}
---
+++ Java/Spoonerizer.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Random;
+
+class Spoonerizer {
+	private ArrayList<String> inputList;
+	private ArrayList<String> headList;
+	private ArrayList<String> tailList;
+	
+	private boolean isVowel(char c) {
+		if (c=='a'||c=='e'||c=='i'||c=='o'||c=='u') {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private void TokenizeInput(String S) throws IOException {
+		inputList = new ArrayList();
+		StringTokenizer st = new StringTokenizer(S);
+		
+		while (st.hasMoreTokens()) {
+			inputList.add(st.nextToken());
+		}
+		inputList.trimToSize();
+		
+		if (inputList.size() < 2) {
+			IOException e = new IOException();
+			throw e;
+		}
+	}
+	
+	private void SplitInput() {
+		String head, tail = "";
+		headList = new ArrayList();
+		tailList = new ArrayList();
+		
+		for (int i = 0; i < inputList.size(); i++) {
+			head = tail = "";
+			String s = inputList.get(i);
+			int j = 0;
+			for (; j < s.length(); j++) {
+				if (!isVowel(s.charAt(j))) {
+					head += s.charAt(j);
+				} else {
+					break;
+				}
+			}
+			for (; j < s.length(); j++) {
+				tail += s.charAt(j);
+			}
+			headList.add(head);
+			tailList.add(tail);
+		}
+		headList.trimToSize();
+		tailList.trimToSize();
+	}
+	
+	private String Recombine() {
+		Random R = new Random();
+		int rand = R.nextInt(inputList.size() - 1) + 1;
+		String output = "";
+		
+		for (int i = 0; i < tailList.size(); i++, rand++) {
+			output += headList.get(rand % headList.size());
+			output += tailList.get(i);
+			if (i != tailList.size() - 1) {
+				output += " ";
+			}
+		}
+		return output;
+	}
+	
+	public String spoon(String s) {
+		try {
+			TokenizeInput(s.toLowerCase());
+		} catch (IOException ioe) {
+			System.out.print("Spoonerizer input error: not enough words.");
+			return "";
+		}
+		SplitInput();
+		String output = Recombine();
+		return output;
+	}
+}
